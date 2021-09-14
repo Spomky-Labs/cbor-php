d8058b1b3a8a1d4b06817dfaf4336e6a1ca94998
Spomky <contact@spomky-labs.com>
2021-09-14 23:14:57 +0200

    Refactoring

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,9 @@
 <?php
 
 $header = 'The MIT License (MIT)
+
 Copyright (c) 2018-2020 Spomky-Labs
+
 This software may be modified and distributed under the terms
 of the MIT license.  See the LICENSE file for details.';
 
