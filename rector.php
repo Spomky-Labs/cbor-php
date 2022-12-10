7479db4398b82f1a46e37beff2fe117a28c1f0f9
Florent Morselli <florent.morselli@spomky-labs.com>
2022-12-10 21:52:38 +0100

    Update: Rector

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
@@ -18,7 +17,4 @@ return static function (RectorConfig $config): void {
     $config->phpVersion(PhpVersion::PHP_80);
     $config->importNames();
     $config->importShortClasses();
-
-    $services = $config->services();
-    $services->set(TypedPropertyRector::class);
 };
