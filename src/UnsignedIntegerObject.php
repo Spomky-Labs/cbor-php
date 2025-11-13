2a5fb86aacfe1004611370ead6caa2bfc88435d0
Florent Morselli <contact@spomky-labs.com>
2025-11-13 14:00:34 +0100

    Remove readonly modifier from properties in various CBOR object classes (#90)
    * Remove readonly modifier from properties in various CBOR object classes
    * Update CI configuration to use actions/checkout@v5 and adjust branch patterns

--- src/UnsignedIntegerObject.php
+++ src/UnsignedIntegerObject.php
@@ -14,7 +14,7 @@ final class UnsignedIntegerObject extends AbstractCBORObject implements Normaliz
 
     public function __construct(
         int $additionalInformation,
-        private readonly ?string $data
+        private ?string $data
     ) {
         parent::__construct(self::MAJOR_TYPE, $additionalInformation);
     }
